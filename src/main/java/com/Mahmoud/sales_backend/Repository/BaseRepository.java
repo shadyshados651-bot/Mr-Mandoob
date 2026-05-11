fcf1532414130af920e6af1291fe5ef8a3967529
Shady_Esmat <shadyshados651@gmail.com>
2026-05-11 13:34:37 +0300

    Update BaseRepository.java

--- src/main/java/com/Mahmoud/sales_backend/Repository/BaseRepository.java
+++ src/main/java/com/Mahmoud/sales_backend/Repository/BaseRepository.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class BaseRepository {
+public abstract class BaseRepository {
 
     protected Connection conn;
 
